a69247efb26908d42d999aaf3fd5230475677042
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-12-26 00:59:38 +0300

    ...

---
+++ Infrastructure/SocialPlatformAPI.Persistence/Contexts/SocialPlatformDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SocialPlatformAPI.Domain.Entities;
+using SocialPlatformAPI.Domain.Entities.Identity;
+
+namespace SocialPlatformAPI.Persistence.Contexts
+{
+    public class SocialPlatformDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public SocialPlatformDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Follow> Follows { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Follow>()
+                .HasOne(f => f.Follower)
+                .WithMany(u => u.Followers)
+                .HasForeignKey(f => f.FollowerId);
+
+            builder.Entity<Follow>()
+                .HasOne(f => f.Following)
+                .WithMany(u => u.Following)
+                .HasForeignKey(f => f.FollowingId);
+
+        }
+
+    }
+}
---
+++ Infrastructure/SocialPlatformAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using SocialPlatformAPI.Persistence.Contexts;
+using SocialPlatformAPI.Domain.Entities.Identity;
+
+namespace SocialPlatformAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SocialPlatformDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")));
+
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<SocialPlatformDbContext>()
+            .AddDefaultTokenProviders();
+
+        }
+    }
+}
